d7bc3944ec3b4908c8152db551281536e5a94791
Van Minh <vanminh91bk@github.com>
2016-04-24 19:11:59 +0700

    Init

---
+++ DemoGame/android/src/com/vanminh/demogame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.vanminh.demogame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.vanminh.demogame.DemoGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DemoGame(), config);
+	}
+}
